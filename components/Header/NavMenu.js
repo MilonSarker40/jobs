19d0b6fc9f34d4365f826427fe4df1e64ea2704b
MilonSarker40 <43253773+MilonSarker40@users.noreply.github.com>
2023-02-10 22:57:21 +0600

    home page fix

--- components/Header/NavMenu.js
+++ components/Header/NavMenu.js
@@ -10,7 +10,7 @@ const NavMenu = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="index">Home</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/about">About Us</Nav.Link>
             <Nav.Link href="#link">Candidated </Nav.Link>
             <NavDropdown title="Employers" id="basic-nav-dropdown">
